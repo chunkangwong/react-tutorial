10c814a0d80ba56181dee632890200d2c16fd851
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-26 03:46:24 +0800

    Added isLoading state to Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,28 +1,41 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import PostsContext, { POSTS_API_URL } from "../contexts/posts.context";
 
 const Post = ({ post: { id, title, body } }) => {
   const { dispatch } = useContext(PostsContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleEditButtonClick = (id) => () => {
     dispatch({ type: "SET_EDITED_POST_ID", payload: id });
   };
 
   const handleDeletePost = (id) => async () => {
-    await fetch(POSTS_API_URL + `/${id}`, {
-      method: "DELETE",
-    });
-    dispatch({ type: "DELETE_POST", payload: id });
+    setIsLoading(true);
+    try {
+      await fetch(POSTS_API_URL + `/${id}`, {
+        method: "DELETE",
+      });
+      dispatch({ type: "DELETE_POST", payload: id });
+    } catch (error) {
+      console.log(error);
+      window.alert("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <div className="post">
       <h2>{title}</h2>
       <p>{body}</p>
-      <button type="button" onClick={handleEditButtonClick(id)}>
-        Edit
+      {!isLoading && (
+        <button type="button" onClick={handleEditButtonClick(id)}>
+          Edit
+        </button>
+      )}
+      <button onClick={handleDeletePost(id)} disabled={isLoading}>
+        {isLoading ? "Deleting post..." : "Delete"}
       </button>
-      <button onClick={handleDeletePost(id)}>Delete</button>
     </div>
   );
 };

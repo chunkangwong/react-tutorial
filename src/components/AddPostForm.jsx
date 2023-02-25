167acd9f7cecfc8589cf5d72d193e8d7217d8aa5
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-26 03:42:57 +0800

    Added isLoading state to AddPostForm

--- src/components/AddPostForm.jsx
+++ src/components/AddPostForm.jsx
@@ -5,6 +5,7 @@ const AddPostForm = () => {
   const { dispatch } = useContext(PostsContext);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -16,21 +17,29 @@ const AddPostForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(POSTS_API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        body,
-        user_id: 1,
-      }),
-    });
-    const newPost = await response.json();
-    dispatch({ type: "ADD_POST", payload: newPost });
-    setTitle("");
-    setBody("");
+    setIsLoading(true);
+    try {
+      const response = await fetch(POSTS_API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          body,
+          user_id: 1,
+        }),
+      });
+      const newPost = await response.json();
+      dispatch({ type: "ADD_POST", payload: newPost });
+    } catch (error) {
+      console.log(error);
+      window.alert("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+      setTitle("");
+      setBody("");
+    }
   };
 
   return (
@@ -49,7 +58,9 @@ const AddPostForm = () => {
         onChange={handleBodyChange}
         required
       />
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? "Adding post..." : "Add"}
+      </button>
     </form>
   );
 };

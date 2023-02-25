9142f2caba6c587f8f3d0170ff6abc34c19b39ab
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-26 02:49:30 +0800

    Fetched posts on initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import AddPostForm from "./components/AddPostForm";
 import EditPostForm from "./components/EditPostForm";
 import Post from "./components/Post";
 
-const staticPosts = [
-  {
-    userId: 1,
-    id: 1,
-    title:
-      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-  },
-  {
-    userId: 1,
-    id: 2,
-    title: "qui est esse",
-    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-  },
-];
+const POSTS_API_URL = import.meta.env.VITE_POSTS_API_URL;
 
 function App() {
-  const [posts, setPosts] = useState(staticPosts);
+  const [posts, setPosts] = useState([]);
   const [editedPostId, setEditedPostId] = useState(null);
+
+  useEffect(() => {
+    fetch(POSTS_API_URL).then((response) => {
+      response.json().then((data) => {
+        setPosts(data);
+      });
+    });
+  }, []);
 
   const handleAddPost = ({ title, body }) => {
     const newPost = {

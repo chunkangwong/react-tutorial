ed0e6fda50730e2159a8f4f8c7961fb2a9fe0553
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-26 04:06:18 +0800

    Post now uses react-query to delete post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,40 +1,47 @@
-import { useContext, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
 import PostsContext, { POSTS_API_URL } from "../contexts/posts.context";
 
 const Post = ({ post: { id, title, body } }) => {
   const { dispatch } = useContext(PostsContext);
-  const [isLoading, setIsLoading] = useState(false);
+  const { isFetching, refetch: deletePost } = useQuery(
+    ["posts"],
+    async () => {
+      const response = await fetch(POSTS_API_URL + `/${id}`, {
+        method: "DELETE",
+      });
+      const deletedPost = await response.json();
+      dispatch({ type: "DELETE_POST", payload: id });
+      return deletedPost;
+    },
+    {
+      onError: (error) => {
+        console.log(error);
+        window.alert("Something went wrong!");
+      },
+      enabled: false,
+    }
+  );
 
-  const handleEditButtonClick = (id) => () => {
+  const handleEditButtonClick = () => {
     dispatch({ type: "SET_EDITED_POST_ID", payload: id });
   };
 
-  const handleDeletePost = (id) => async () => {
-    setIsLoading(true);
-    try {
-      await fetch(POSTS_API_URL + `/${id}`, {
-        method: "DELETE",
-      });
-      dispatch({ type: "DELETE_POST", payload: id });
-    } catch (error) {
-      console.log(error);
-      window.alert("Something went wrong!");
-    } finally {
-      setIsLoading(false);
-    }
+  const handleDeletePost = async () => {
+    await deletePost();
   };
 
   return (
     <div className="post">
       <h2>{title}</h2>
       <p>{body}</p>
-      {!isLoading && (
-        <button type="button" onClick={handleEditButtonClick(id)}>
+      {!isFetching && (
+        <button type="button" onClick={handleEditButtonClick}>
           Edit
         </button>
       )}
-      <button onClick={handleDeletePost(id)} disabled={isLoading}>
-        {isLoading ? "Deleting post..." : "Delete"}
+      <button onClick={handleDeletePost} disabled={isFetching}>
+        {isFetching ? "Deleting post..." : "Delete"}
       </button>
     </div>
   );

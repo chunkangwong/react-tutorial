e45c3e67b2acdbf3c05014635b9ebfb940e7556c
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-26 13:12:40 +0800

    Post now uses useMutation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useContext } from "react";
 import PostsContext, { POSTS_API_URL } from "../contexts/posts.context";
 
 const Post = ({ post: { id, title, body } }) => {
   const { dispatch } = useContext(PostsContext);
-  const { isFetching, refetch: deletePost } = useQuery(
-    ["posts"],
-    async () => {
-      const response = await fetch(POSTS_API_URL + `/${id}`, {
+  const queryClient = useQueryClient();
+  const { mutate: deletePost, isFetching } = useMutation({
+    mutationFn: async () => {
+      await fetch(POSTS_API_URL + `/${id}`, {
         method: "DELETE",
       });
-      const deletedPost = await response.json();
-      dispatch({ type: "DELETE_POST", payload: id });
-      return deletedPost;
     },
-    {
-      onError: (error) => {
-        console.log(error);
-        window.alert("Something went wrong!");
-      },
-      enabled: false,
-    }
-  );
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["posts"],
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+      window.alert("Something went wrong!");
+    },
+  });
 
   const handleEditButtonClick = () => {
     dispatch({ type: "SET_EDITED_POST_ID", payload: id });
   };
 
-  const handleDeletePost = async () => {
-    await deletePost();
+  const handleDeletePost = () => {
+    deletePost();
   };
 
   return (

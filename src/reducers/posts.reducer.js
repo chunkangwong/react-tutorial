d7499ee3faf4c89c009d63e2ab27b03235467ee6
Wong Chun Kang <chunkang.wong94@gmail.com>
2023-02-26 13:21:13 +0800

    Removed unused posts prop from posts.reducer.js

--- src/reducers/posts.reducer.js
+++ src/reducers/posts.reducer.js
@@ -1,15 +1,9 @@
 export const initialState = {
-  posts: [],
   editedPostId: null,
 };
 
 const postsReducer = (state, action) => {
   switch (action.type) {
-    case "SET_POSTS":
-      return {
-        ...state,
-        posts: action.payload,
-      };
     case "SET_EDITED_POST_ID":
       return {
         ...state,
